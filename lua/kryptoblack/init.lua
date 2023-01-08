e80df705c6fef597bd5f0b4a1899330a22148a29
Pratik Thakare <patrick.thakare123@gmail.com>
2023-01-08 18:07:20 +0530

    first commit

---
+++ lua/kryptoblack/init.lua
@@ -0,0 +1,2 @@
+require("kryptoblack.remap")
+require("kryptoblack.set")
---
+++ lua/kryptoblack/set.lua
@@ -0,0 +1,39 @@
+-- fat cursor
+vim.opt.guicursor = ""
+
+-- line numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- tab spacing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+-- disable swapfile and backup
+-- rely on undotree to store everylast change
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/vim/undodir"
+vim.opt.undofile = true
+
+-- string search in a file
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
